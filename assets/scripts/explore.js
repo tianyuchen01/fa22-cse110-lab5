afad6f0a04368ff85c496ee10d1a7243fcd5e8c3
Tianyu Chen <tianyuchen1016@gmail.com>
2022-10-30 20:35:52 -0700

    SpeechSynthesis complete

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,51 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  // populate voice selection dropdown
+  const synth = window.speechSynthesis;
+  let voiceSelect = document.querySelector("select");
+  let voices = [];
+
+  synth.addEventListener("voiceschanged", function() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  });  
+
+  // press button effect
+  let textInput = document.getElementById("text-to-speak");
+  let btn = document.querySelector("button");
+
+  btn.addEventListener("click", function() {
+    const utterThis = new SpeechSynthesisUtterance(textInput.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+
+    // change face when speaking
+    let face = document.querySelector("img");
+    utterThis.addEventListener("start", function() {
+      face.setAttribute("src", "assets/images/smiling-open.png");
+    });
+    utterThis.addEventListener("end", function() {
+      face.setAttribute("src", "assets/images/smiling.png");
+    });
+  });
+
 }
